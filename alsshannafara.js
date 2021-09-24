fd01171835c9330091ac637efb12c70106d00059
alshanfara <86086133+alshanfara@users.noreply.github.com>
2021-09-24 20:49:06 +0300

    Update and rename README.md to alsshannafara.js

---
+++ alsshannafara.js
@@ -0,0 +1,10 @@
+<img alt="ابحث على قناتنا فى اليوتيوب" src="https://3.bp.blogspot.com/-8hzm4CcaOVU/XG_f4Uf7rzI/AAAAAAAAAak/6Nt8xQ5s7PcvlAxUweVu4UDgZ2LvZjYsACLcBGAs/s1600/serach-box-youtube-arabes1.png" />
+<br />
+<form id="searchThis" action="https://www.youtube.com/user/infoarabes1/search?query=" style="display: inline;" target="_blank" method="get">
+<input is="iron-input" slot="input" class="input-element style-scope paper-input" autocomplete="on" placeholder="" autocapitalize="none" autocorrect="on" aria-describedby="" aria-labelledby="paper-input-label-1" name="query" tabindex="0" id="input-1" />
+<input id="searchButton" value="Search" type="submit" /></form>
+<style>
+input#input-1 {width: 75%;}
+ </div> 
+     <center>
+`).insertBefore("#d2bc");
